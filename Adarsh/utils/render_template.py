76dccde18ed86f23e9e86ead11aaf9e66ab1cc41
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2023-09-11 16:18:17 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -8,29 +8,32 @@ import aiofiles
 import logging
 import aiohttp
 
-
 async def render_page(id, secure_hash):
-    file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
+    file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
         logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
         logging.debug(f"Invalid hash for message with - ID {id}")
         raise InvalidHash
     src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(id)}')
+    
+    # Remove underscores from file_data.file_name
+    file_name_no_underscores = file_data.file_name.replace("_", " ")
+    
     if str(file_data.mime_type.split('/')[0].strip()) == 'video':
         async with aiofiles.open('Adarsh/template/req.html') as r:
-            heading = 'Watch {}'.format(file_data.file_name)
+            heading = 'Watch {}'.format(file_name_no_underscores)  # Use modified file name
             tag = file_data.mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+            html = (await r.read()).replace('tag', tag) % (heading, file_name_no_underscores, src)
     elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
         async with aiofiles.open('Adarsh/template/req.html') as r:
-            heading = 'Listen {}'.format(file_data.file_name)
+            heading = 'Listen {}'.format(file_name_no_underscores)  # Use modified file name
             tag = file_data.mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+            html = (await r.read()).replace('tag', tag) % (heading, file_name_no_underscores, src)
     else:
         async with aiofiles.open('Adarsh/template/dl.html') as r:
             async with aiohttp.ClientSession() as s:
                 async with s.get(src) as u:
-                    heading = 'Download {}'.format(file_data.file_name)
+                    heading = 'Download {}'.format(file_name_no_underscores)  # Use modified file name
                     file_size = humanbytes(int(u.headers.get('Content-Length')))
-                    html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+                    html = (await r.read()) % (heading, file_name_no_underscores, src, file_size)
     return html
